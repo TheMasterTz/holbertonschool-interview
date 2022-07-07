a5d2a4b48de4ca8eb412325cf660537f2c79a2de
TheMasterTz <palavras2001@hotmail.com>
2022-07-07 12:27:49 -0500

    [ADD: task 0]

--- 0x0B-menger/menger.h
+++ 0x0B-menger/menger.h
@@ -1,10 +1,11 @@
 #ifndef MENGER_H
 #define MENGER_H
 
-void menger(int level);
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
 #include <math.h>
+
+void menger(int level);
 
 #endif/*MENGER*/
